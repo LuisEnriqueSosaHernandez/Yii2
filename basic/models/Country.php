9c0346439a7c1ad5ab21e1e754dcb32b9727bbe1
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-02-07 15:11:07 -0600

    basic

--- basic/models/Country.php
+++ basic/models/Country.php
@@ -1,8 +1,48 @@
 <?php
+
 namespace app\models;
-use yii\db\ActiveRecord;
 
-class Country extends ActiveRecord{
+use Yii;
 
+/**
+ * This is the model class for table "country".
+ *
+ * @property string $code
+ * @property string $name
+ * @property int $population
+ */
+class Country extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['population'], 'required'],
+            [['population'], 'integer'],
+            [['code', 'name'], 'string', 'max' => 45],
+            [['population'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
 }
-?>
