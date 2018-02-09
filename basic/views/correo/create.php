eb54ea71e1021fbc6e4bafd3d482e6dde241e9c0
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-02-09 16:52:54 -0600

    advanced

--- basic/views/correo/create.php
+++ basic/views/correo/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Correo */
 
-$this->title = 'Create Correo';
+$this->title = 'Create Correo ahora xD';
 $this->params['breadcrumbs'][] = ['label' => 'Correos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
