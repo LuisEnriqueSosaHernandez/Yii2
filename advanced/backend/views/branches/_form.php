d73273bc77ca076c8384e7b59c277d8d4cad5a61
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-02-20 16:43:04 -0600

    leeson 10

--- advanced/backend/views/branches/_form.php
+++ advanced/backend/views/branches/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\Companies;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Branches */
@@ -11,8 +13,10 @@ use yii\widgets\ActiveForm;
 <div class="branches-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'companies_company_id')->textInput() ?>
+    
+    <?= $form->field($model, 'companies_company_id')->dropDownList(ArrayHelper::map(Companies::find()->all()
+    ,'company_id','company_name'),['prompt'=>'Select Company']
+    ) ?>
 
     <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
 
