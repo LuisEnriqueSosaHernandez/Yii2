573733b0672faf0b74c74b911c3e09b676810fad
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-03-03 21:05:38 -0600

    branches

--- advanced/backend/views/branches/index.php
+++ advanced/backend/views/branches/index.php
@@ -22,6 +22,13 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'rowOptions'=>function($model){
+                        if($model->branch_status=="inactive"){
+                            return['class'=>'danger'];
+                        }else if($model->branch_status=="active"){
+                             return['class'=>'success'];
+                        }
+        },
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
             [
